ec9ba89933e84a351cfbe29384d107dd49f876e7
lizhihua <lizhihua@mobike.com>
2019-11-06 15:00:44 +0800

    获取最近1小时被删除的热搜

--- src/main/java/com/webcollector/webcollector/service/TopServiceImpl.java
+++ src/main/java/com/webcollector/webcollector/service/TopServiceImpl.java
@@ -135,7 +135,7 @@ public class TopServiceImpl implements TopService{
         topList.sort(Comparator.comparing(Top::getHeat).reversed());
         localCache.put(LocalCache.GETTOP,topList);
 
-        List<Top> deletedTop1 = findDeletedTop(100);
+        List<Top> deletedTop1 = topDao.findLastMinuteDeleted(getLastMinute(60));
         localCache.put(LocalCache.FINDDELETETOP,deletedTop1);
     }
 }
