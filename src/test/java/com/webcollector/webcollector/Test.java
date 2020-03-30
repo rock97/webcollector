b858af15bcd6bafa7785b01627fe15d5868639c4
lizhihua <lizhihua@mobike.com>
2020-03-30 23:59:28 +0800

    词频分析其

--- src/test/java/com/webcollector/webcollector/Test.java
+++ src/test/java/com/webcollector/webcollector/Test.java
@@ -16,179 +16,87 @@ public class Test {
     public static void wordFrequency() throws IOException {
 
         Map<String, Integer> map = new HashMap<>();
-
-
-
         String article = getString();
-
         String result = ToAnalysis.parse(article).toStringWithOutNature();
-
         String[] words = result.split(",");
-
-
-
-
-
         for(String word: words){
-
             String str = word.trim();
-
             // 过滤空白字符
-
             if (str.equals(""))
-
                 continue;
-
                 // 过滤一些高频率的符号
-
             else if(str.matches("[）|（|.|，|。|+|-|“|”|：|？|\\s]"))
-
                 continue;
-
                 // 此处过滤长度为1的str
-
             else if (str.length() < 2)
-
                 continue;
-
-
-
             if (!map.containsKey(word)){
-
                 map.put(word, 1);
-
             } else {
-
                 int n = map.get(word);
-
                 map.put(word, ++n);
-
             }
-
         }
-
-
-
         Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-
         while (iterator.hasNext()){
-
             Map.Entry<String, Integer> entry = iterator.next();
-
             //System.out.println(entry.getKey() + ": " + entry.getValue());
-
         }
-
-
-
         List<Map.Entry<String, Integer>> list = new ArrayList<>();
-
         Map.Entry<String, Integer> entry;
-
         while ((entry = getMax(map)) != null){
-
             list.add(entry);
-
         }
-
         System.out.println(Arrays.toString(list.toArray()));
-
         list.forEach(v->{
             System.out.println(v.getKey()+" = " + v.getValue());
         });
 
     }
 
-
-
-
-
     /**
-
      * 找出map中value最大的entry, 返回此entry, 并在map删除此entry
-
      * @param map
-
      * @return
-
      */
-
     public static Map.Entry<String, Integer> getMax(Map<String, Integer> map){
-
         if (map.size() == 0){
-
             return null;
-
         }
-
         Map.Entry<String, Integer> maxEntry = null;
-
         boolean flag = false;
-
         Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-
         while (iterator.hasNext()){
-
             Map.Entry<String, Integer> entry = iterator.next();
-
             if (!flag){
-
                 maxEntry = entry;
-
                 flag = true;
-
             }
-
             if (entry.getValue() > maxEntry.getValue()){
-
                 maxEntry = entry;
-
             }
-
         }
-
         map.remove(maxEntry.getKey());
-
         return maxEntry;
-
     }
-
-
-
     /**
-
      * 从文件中读取待分割的文章素材.
-
      　　* 文件内容来自简书热门文章: https://www.jianshu.com/p/5b37403f6ba6
-
      * @return
-
      * @throws IOException
-
      */
 
     public static String getString() throws IOException {
 
         FileInputStream inputStream = new FileInputStream(new File("/Users/lizhihuamobike/text.txt"));
-
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
         StringBuilder strBuilder = new StringBuilder();
-
-
-
         String line;
-
         while((line = reader.readLine()) != null){
-
             strBuilder.append(line);
-
         }
-
         reader.close();
-
         inputStream.close();
-
         return strBuilder.toString();
 
     }
